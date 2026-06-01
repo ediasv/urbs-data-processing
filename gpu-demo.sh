c22d4a1f6f3f279fb9ad9fe79e868f2be23b83cf
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2026-06-01 10:51:01 -0400

    wip

--- gpu-demo.sh
+++ gpu-demo.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+module load StdEnv/2023
+module load cudacore/.12.2.2
+module load java/17.0.6
+module load spark/3.5.6
+module load python/3.10
+
+virtualenv -p python3.10 venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install -e .
+
 START_DATE="2026-04-16"
 END_DATE="2026-04-18"
 YEAR_MONTH="${START_DATE:0:7}"
