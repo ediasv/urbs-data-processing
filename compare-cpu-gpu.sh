71a76cc98b5ed8f57c864a7950af3ab0587b512c
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2026-06-02 16:56:59 -0400

    wip

--- compare-cpu-gpu.sh
+++ compare-cpu-gpu.sh
@@ -10,25 +10,51 @@ module load python/3.10
 
 source venv/bin/activate
 
-export EVENT_LOG_DIR="./tmp/spark-events"
 export START_DATE="2024-08-01"
 export END_DATE="2024-08-31"
 export YEAR_MONTH="${START_DATE:0:7}"
-export EVENT_LOG_DIR=/tmp/spark-events
 
-time spark-submit \
-  --master local[2] \
-  --executor-memory 8G \
-  --driver-memory 8G \
-  dataprocessing/job/refined_ingestion.py -ds "$START_DATE" -de "$END_DATE" -j tracking
+# Download URBS Data
+echo "Downloading URBS data..."
+python dataprocessing/job/download_files.py -s "$START_DATE" -e "$END_DATE" -fd linhas -fl linhas.json.xz
+python dataprocessing/job/download_files.py -s "$START_DATE" -e "$END_DATE" -fd pontoslinha -fl pontosLinha.json.xz
+python dataprocessing/job/download_files.py -s "$START_DATE" -e "$END_DATE" -fd veiculos -fl veiculos.json.xz
 
-time spark-submit \
-    --master local[2] \
+# Uncompress URBS Data
+echo "Decompressing URBS data..."
+python dataprocessing/job/decompress_files.py -s "$START_DATE" -e "$END_DATE" -fd linhas -fl linhas.json.xz
+python dataprocessing/job/decompress_files.py -s "$START_DATE" -e "$END_DATE" -fd pontoslinha -fl pontosLinha.json.xz
+python dataprocessing/job/decompress_files.py -s "$START_DATE" -e "$END_DATE" -fd veiculos -fl veiculos.json.xz
+
+# Execute trusting processor
+echo "Processing trusting data..."
+spark-submit \
+    --master local[12] \
     --executor-memory 8G \
     --driver-memory 8G \
-    --jars rapids-4-spark_2.12-26.04.2.jar \
-    --conf spark.driver.extraClassPath=rapids-4-spark_2.12-26.04.2.jar \
-    --conf spark.executor.extraClassPath=rapids-4-spark_2.12-26.04.2.jar \
-    --conf spark.plugins=com.nvidia.spark.SQLPlugin \
-    --conf spark.rapids.sql.incompatibleDateFormats.enabled=true \
-    dataprocessing/job/refined_ingestion.py -ds "$START_DATE" -de "$END_DATE" -j tracking
+    dataprocessing/job/trust_ingestion.py -d "$YEAR_MONTH"
+
+# Execute refined processor: Dimensions (CPU ONLY)
+echo "Processing refined dimension data on CPU..."
+spark-submit \
+    --master local[12] \
+    --executor-memory 8G \
+    --driver-memory 8G \
+    dataprocessing/job/refined_ingestion.py -ds "$START_DATE" -de "$END_DATE" -j line
+
+spark-submit \
+    --master local[12] \
+    --executor-memory 8G \
+    --driver-memory 8G \
+    dataprocessing/job/refined_ingestion.py -ds "$START_DATE" -de "$END_DATE" -j itinerary
+
+# time spark-submit \
+#     --master local[2] \
+#     --executor-memory 8G \
+#     --driver-memory 8G \
+#     --jars rapids-4-spark_2.12-26.04.2.jar \
+#     --conf spark.driver.extraClassPath=rapids-4-spark_2.12-26.04.2.jar \
+#     --conf spark.executor.extraClassPath=rapids-4-spark_2.12-26.04.2.jar \
+#     --conf spark.plugins=com.nvidia.spark.SQLPlugin \
+#     --conf spark.rapids.sql.incompatibleDateFormats.enabled=true \
+#     dataprocessing/job/refined_ingestion.py -ds "$START_DATE" -de "$END_DATE" -j tracking
